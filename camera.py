64ca2a8c003c3b9a1fdc1acd663c16802f2eae08
PedroHteles <pedroteles849@gmail.com>
2021-10-08 09:32:02 -0300

    v2

--- camera.py
+++ camera.py
@@ -1,85 +1,97 @@
+
+from ctypes import resize
+from os import O_TRUNC, closerange
 import cv2
-from numpy.lib.function_base import append
+from numpy.core.numeric import isclose
 from pyzbar.pyzbar import decode
 import json
 import numpy as np
 
+banco = ({'end': 0, 'e': 21}, {'p': 10, 'e': 21})
+validado = []
+naoValido = []
 
-qrlidos = []
-enderecoLidos = []
-produtosLidos = []
-
-banco = [{'p': 0, 'e': 112}, {'p': 10, 'e': 112}]
+leituraProduto = []
+leituraEndereco = []
+area = []
 
 def lerqr(x,height,width):
-    start_point = (int(width / 6), int(height  / 6))
-    end_point = (int(width / 1.20), int(height / 1.20))
+    start_point = (int(width / 8), int(height  / 8))
+    end_point = (int(width / 1.15), int(height / 1.15))
     cv2.rectangle(img, start_point, end_point,(255,0,0),2)
     
-
     for barcode in decode(x):
         (x, y, w, h) = barcode.rect
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
         leituraArea = x < (end_point[0] - w) and x > start_point[0] and y < (end_point[1] - h ) and y > start_point[1] 
-        
         try:
             qr = json.loads(barcodeData)
-            enderecoQr = qr['e']
-            qrCodigoProduto = qr['p']
-            tesss = len(decode(img))
-            if leituraArea: 
-                if qr not in qrlidos:
-                    qrlidos.append(qr)
-                    return
+            qrEnd = 'end' in qr
+            qrProd = 'p' in qr
+            if leituraArea:
+                if qrEnd and qr not in leituraEndereco:
+                    leituraEndereco.append(qr) 
+                elif  qrProd and qr not in leituraProduto:
+                    leituraProduto.append(qr)
 
-
-                if tesss == 2 and len(qrlidos) == 2:
-                    print(qrlidos,qrlidos == banco)
-
-                elif tesss > 2 and len(qrlidos) > 2:
-                    for i in qrlidos:
-                        if i['p'] == 0:
-                            if i not in enderecoLidos:
-                                enderecoLidos.append(i)
-
-                    if len(enderecoLidos) == 1 and  enderecoLidos !=[]:            
-                        for i in qrlidos:
-                            if i['p'] > 0:
-                                if i not in produtosLidos:
-                                    produtosLidos.append(i)
-                            if len(produtosLidos) > 1:
-                                if qr in  produtosLidos:
-                                    print('mais de 1 produto encontrado!',produtosLidos)
-                                    return produtosLidos
-                    elif len(enderecoLidos) > 1:
-                        if qr in enderecoLidos:
-                            print('mais de 1 endereco encontrado!',enderecoLidos)
-            
+                if len(decode(img)) > 2:
+                    print(' mais de 2 qr encontrado')
+                    if len(leituraEndereco) + len(leituraProduto) == len(decode(img)):
+                        valor = (leituraEndereco,leituraProduto)
+                        return valor     
+                else:
+                    pass
+                if len(decode(img)) == 1:
+                    if qrEnd and len(leituraProduto) == 1:
+                        leituraProduto.clear()
+                    elif qrProd and len(leituraEndereco) == 1:
+                        leituraEndereco.clear()
+                    if len(leituraEndereco) > 1:
+                        leituraEndereco.clear()
+                    elif len(leituraProduto) >1:
+                        leituraProduto.clear()
+               
+                elif len(decode(img)) == 2:
+                    if qrProd and len(leituraEndereco) != 1:
+                        leituraEndereco.clear()
+                    elif qrEnd and len(leituraProduto) != 1:
+                        leituraProduto.clear()
+                    if qrEnd and len(leituraEndereco) == 2:
+                        leituraProduto.clear()
+                    elif qrProd and len(leituraProduto) == 2:
+                        leituraEndereco.clear()
+     
+                if len(leituraEndereco) + len(leituraProduto) == len(decode(img)):
+                    if len(leituraEndereco) == 1 and len(leituraProduto) == 0:
+                        print('aguardando Produto')
+                        leituraEndereco.clear()
+                    elif len(leituraProduto) == 1 and len(leituraEndereco) == 0:
+                        print('aguardando Endereco')
+                        leituraProduto.clear()
+                    elif len(leituraEndereco) > 1:
+                        print('mais de 1 endereco encontrado!') 
+                    elif len(leituraProduto) > 1:
+                        print(' mais de 1 produto encontrado')
+                    else:
+                        valor = (leituraEndereco,leituraProduto)
+                        return valor  
             else:
-                tesss - 1
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0,255), 20)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0,0,255), 15)                   
         except:
-            if(leituraArea):
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0,255, 255), 2)
 while True:
     img = cv2.imread('./img/dirnal.png')
     height, width, channels = img.shape
-    lerqr(img,height,width)
-    
-  
+    result = lerqr(img,height,width)
+    print(result)
+    if result != None and len(result) == 2:
+        if result[0][0]['e'] == result[1][0]['e'] and len(result) == 2:
+            if result not in validado:
+                validado.append(result)
+    print(result,validado)
+
     cv2.imshow("camera",img)
     key = cv2.waitKey(5)
     if key == 27:
         break
-    if key == 122:
-        enderecoLidos.clear()
-        produtosLidos.clear()
-        qrlidos.clear()
-
-
-    
-
-        
